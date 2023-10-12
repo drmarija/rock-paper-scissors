472c9d64d97d0ca5f29d10e9ecbeb7b31fcddc5c
drmarija <>
2023-10-12 17:39:03 +0000

    Add DOM element,functions for computer and player to choose different options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log("connected");
+// Getting the DOM elements
+let playerScore = 0;
+let computerScore = 0;
+
+const choices = document.getElementsByClassName("choices");
+const pScore = document.getElementsByClassName("player-score");
+const cScore = document.getElementsByClassName("computer-score");
+const cpuChoice = document.getElementById("computer-choice");
+const restartBtn = document.getElementById("restart");
+
+// Function for computer to generate the random choices
+
+const options = ["rock", "paper", "scissors"];
+function computerPlay() {
+    const computerIndex = Math.floor(Math.random() * 3);
+    return options[computerIndex];
+}
+
+// Function for player to choose the option 
+
+function playerPlay() {
+    const playerChoice = this.id;
+    const computerChoice= computerPlay();
+    updateScore(playerChoice, computerChoice);
+}
+
+choices.forEach((choice) => choice.addEventListener("click", playerPlay));
